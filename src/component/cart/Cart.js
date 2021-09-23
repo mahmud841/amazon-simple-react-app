9dc2617ccd32a30b095d14e98d680e81628d7400
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-23 22:59:59 +0800

    second commit

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -4,18 +4,26 @@ import './Cart.css';
 const Cart = (props) => {
   // console.log(props.cart);
   const {cart} = props;
-  let total = 0;
+  const total = cart.reduce((previous, product )=> previous + product.price, 0 );
+ /*  let total = 0;
   for (const product of cart) {
     total = total + product.price;
-  }
+  } */
   
+
+  const shipping = 15;
+  const tax = (total + shipping) * 10; 
+  const grandTotal = (total + shipping + tax);
   return (
-    <div>
+    <div className="order-product">
         <h3>  Order Summary</h3>
-        <h3>Items ordersed: {props.cart.length}</h3>
+        <h3>Items ordered: {props.cart.length}</h3>
         {/* <h5>Items Ordered: {cart.length} </h5> */}
         <br />
-        <p>Total:{total} </p>
+        <p>Total:{total.toFixed(3)} </p>
+        <p>Shipping:{shipping} </p>
+        <p>Tax:{tax.toFixed(3)} </p>
+        <p>Grand Total: {grandTotal.toFixed(3)} </p>
     </div>
   );
 };
